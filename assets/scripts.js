30f843aeb609a815491e9af5a68aaf4df3d3a5d0
Kenneth Kisakye <kanyecope@gmail.com>
2023-06-22 16:58:13 +0300

    Handle button clicks and save

--- assets/scripts.js
+++ assets/scripts.js
@@ -6,6 +6,11 @@
 
     let countClicks = () => {
       $("#tech_btn").on("click", function () {
+        let count = 0;
+        let value_count = $("#display_clicks").val();
+        console.log("----", value_count);
+        $("#display_clicks").html(99);
+
         let data = {
           action: "kc_count_btn_clicks",
           current_value: 0,
--- includes/class-manage-tasks.php
+++ includes/class-manage-tasks.php
@@ -25,11 +25,22 @@ class Tech_Tasks
 
     function kc_button($content)
     {
+        global $post;
+        $user_id =  get_current_user_id();
+        $user_clicks = get_user_meta($user_id, 'kc_button_click_counts', true);
+        $counts = $user_clicks ? $user_clicks : 0;
+
         $content .= '  <button class="button" id="tech_btn">Click me!</button>';
+        $content .= '  <label> <span id="display_clicks">' . $counts . ' </span> Click</label>';
         return $content;
     }
 
     function handle_button_clicks()
     {
+        $user_id =  get_current_user_id();
+        if (isset($_POST['current_value'])) {
+            $btn_count = sanitize_text_field($_POST['current_value']);
+            update_user_meta($user_id, 'kc_button_click_counts', $btn_count);
+        }
     }
 }
